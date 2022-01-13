14e2fc93a390ba38bf8cde535f6da9922aaf83b1
juanjo2708 <juanjo2708@gmail.com>
2022-01-13 09:47:40 +0100

    2022.01.13 matí

--- BE_java/SpringBoot_jwt_1/src/main/java/academy/it/SpringBoot_jwt_1/filter/CustomAuthenticationFilter.java
+++ BE_java/SpringBoot_jwt_1/src/main/java/academy/it/SpringBoot_jwt_1/filter/CustomAuthenticationFilter.java
@@ -1,10 +1,15 @@
 package academy.it.SpringBoot_jwt_1.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -12,6 +17,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -23,7 +31,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         this.authenticationManager = authenticationManager;
     }
 
-    // Autenticació
+    // Autenticació:  L'usuari envia user i password
     // Crida al gestor d'autenticació passant les credencials de l'usuari
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -35,17 +43,37 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
         //Creem un objecte token d'autenticació de contrasenya de nom d'usuari.
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
-        // Retornem el testimoni d'autenticació
+        // Retornem el testimoni d'autenticació al ma
         return authenticationManager.authenticate(authenticationToken);
 
 
     }
 
-    //Autorització
     // mètode que s'executa si ha tingut èxit l'autenticació.
-    //Enviar el testimoni d'accés i d'actualització quan l'inici de sessió sigui exitòs
+    //Envia el testimoni(token) en el header al client pq el pugui emmagatzemar
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        User user = (User) authResult.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+        String access_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+ 10*60*1000))   // Expira als deu minuts
+                .withIssuer(request.getRequestURL().toString())                    // Emisor
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors))
+                .sign(algorithm);
+
+        String refresh_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+ 30*60*1000))   // Expira als deu minuts
+                .withIssuer(request.getRequestURL().toString())                    // Emisor
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors))
+                .sign(algorithm);
+
+        response.setHeader("access_token", access_token);
+        response.setHeader("refresh_token", refresh_token);
     }
+
+
+
+
 }
--- BE_java/SpringBoot_jwt_1/src/main/java/academy/it/SpringBoot_jwt_1/security/SecurityConfig.java
+++ BE_java/SpringBoot_jwt_1/src/main/java/academy/it/SpringBoot_jwt_1/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package academy.it.SpringBoot_jwt_1.security;
 
 import academy.it.SpringBoot_jwt_1.filter.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,9 +32,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-       http.csrf().disable(); // Desactivar la falsificació de sol.licituds entre llocs.
+       http.csrf().disable(); // Desactivar el passo de tokens entre llocs que poden ser falsificats.
        http.sessionManagement().sessionCreationPolicy(STATELESS);  //Política de creació de gestió de sessions http, sense estat.
-       http.authorizeHttpRequests().anyRequest().permitAll(); // http autoritza qualsevol sol.licitud que ho permeti tot
-       http.addFilter(new CustomAuthenticationFilter);  // Afegim un filtre (CustomAutenticationFilter) d'autenticació per poder comprovar l'usuari
+       http.authorizeHttpRequests().anyRequest().permitAll(); // http autoritza qualsevol sol.licitud
+       http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));  // Afegim un filtre (CustomAutenticationFilter) d'autenticació per poder comprovar l'usuari
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
     }
 }
